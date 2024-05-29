58de75f6681737fab971fbcbc2d72f5b3857bbc0
Gonçalo Matias <goncalorosa.matias@gmail.com>
2024-05-29 12:54:45 +0100

    Pormenores

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,35 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Movies</title>
-    <style>
-        table,
-        th,
-        td {
-           border: 1px solid black;
-         border-cosllapse: collapse;      
-        }
-    </style>
-</head>
-<body>
-    <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Genre</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($movies as $movie)    
-                <tr>
-                <td>{{ $movie->title}}</td>
-                <td>{{ $movie->genre_code}}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</body>
-</html>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Movies') }}
+        </h2>
+    </x-slot>
+    
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Genre</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($movies as $movie)    
+                                <tr>
+                                <td>{{ $movie->title}}</td>
+                                <td>{{ $movie->genre_code}}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
+Route::get('movies', [MovieController::class, 'index']);
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -19,5 +21,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-Route::get('movies', [MovieController::class, 'index']);
