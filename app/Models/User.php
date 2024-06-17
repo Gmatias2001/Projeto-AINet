bb1c2a3dbc1cd157ce0d310f4344e60e8a38fefa
Gonçalo Matias <goncalorosa.matias@gmail.com>
2024-06-17 09:53:02 +0100

    MustVerifyEmail

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\Storage;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
@@ -49,11 +49,6 @@ class User extends Authenticatable
         ];
     }
 
-    public function customerRef():HasOne
-    {
-       return $this->hasOne(Customer::class,'id','id');
-    }
-
     public function getPhotoFullUrlAttribute()
     {
         debug($this->photo_filename);
@@ -64,4 +59,11 @@ class User extends Authenticatable
             return asset("storage/photos/anonymous.png");
         }
     }
+
+    public function customerRef():HasOne
+    {
+       return $this->hasOne(Customer::class,'id','id');
+    }
+
+
 }
